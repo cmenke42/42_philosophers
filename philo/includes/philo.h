ab160f3c5cf7d5269f6e4def50cb9bb2fda6af58
cmenke <cmenke@student.42.fr>
2023-08-09 15:33:51 +0000

    added main, fr_isdigit and ft_atoi

---
+++ philo/includes/philo.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cmenke <cmenke@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/08 23:59:08 by cmenke            #+#    #+#             */
+/*   Updated: 2023/08/09 00:16:52 by cmenke           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdbool.h>
+
+#endif 
---
+++ philo/sources/input_validation.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   input_validation.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cmenke <cmenke@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/09 00:16:40 by cmenke            #+#    #+#             */
+/*   Updated: 2023/08/09 15:33:28 by cmenke           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/philo.h"
+
+// argv[1] = number_of_philosophers
+// argv[2] = time_to_die
+// argv[3] = time_to_eat
+// argv[4] = time_to_sleep
+// (argv[5] = number_of_times_each_philosopher_must_eat) - optional argument
+
+
+bool input_validation(int argc, char **argv)
+{
+
+}
+
